b7d4ddb8e2759aa8c751c14049abbe2a4409f722
neeraj-dev002 <neeraj.dev002@gmail.com>
2026-07-15 17:25:16 +0530

    add Employee model with fillable attributes for employee data

---
+++ app/Models/Employee.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    //
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'gender',
+        'department',
+        'position',
+        'joining_date',
+        'status',
+        'address',
+        'salary'
+    ]
+}
