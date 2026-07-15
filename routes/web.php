e9a3261455b6dce2265fdac03829a64a20fa64a8
neeraj-dev002 <neeraj.dev002@gmail.com>
2026-07-15 17:24:57 +0530

    update routes to include employee and department views

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,25 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+
+
+Route::get("/employees",function(){
+    return view("/employees/index");
+     
+});
+
+Route::get("/employees/create",function(){
+    return view("/employees/create");
+});
+
+Route::get("/employees/{id}",function($id){
+   return view("/employees/show",["id"=>$id]);
+});
+
+
+Route::get("/departments",function(){
+   return view("/departments/index");
 });
