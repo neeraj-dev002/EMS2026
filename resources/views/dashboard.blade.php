f3888809d8a26861363359e5a05eb1cc5d1bd50a
neeraj-dev002 <neeraj.dev002@gmail.com>
2026-07-20 17:36:05 +0530

    refactor: update employee table to dynamically display employee details and status

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -101,18 +101,30 @@
                         </thead>
 
                         <tbody>
+@foreach ($employees as $employee )
+
 
                         <tr class="border-b hover:bg-gray-50">
-                            <td class="px-6 py-4">EMP001</td>
-                            <td class="px-6 py-4">Rahul Sharma</td>
-                            <td class="px-6 py-4">IT</td>
+                            <td class="px-6 py-4">{{$employee->id}}</td>
+                            <td class="px-6 py-4">{{$employee->name}}</td>
+                            <td class="px-6 py-4">{{$employee->department->dep_name}}</td>
 
                             <td class="px-6 py-4">
-                                <span class="bg-green-100 text-green-700 px-3 py-1 rounded-full text-sm">
+                             @if($employee->status ==1)   
+                             <span class="bg-green-100 text-green-700 px-3 py-1 rounded-full text-sm">
                                     Active
                                 </span>
+                                @else
+                                    <span class="bg-red-100 text-red-700 px-3 py-1 rounded-full text-sm">
+                                    Inactive
+                                </span>
+                                @endif  
+
+                                
                             </td>
                         </tr>
+
+                        @endforeach
 
                         <tr class="border-b hover:bg-gray-50">
                             <td class="px-6 py-4">EMP002</td>
