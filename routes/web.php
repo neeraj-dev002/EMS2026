11257d9e8f1e3db09471bd5a08548bf20b2eca85
neeraj-dev002 <neeraj.dev002@gmail.com>
2026-07-20 13:56:54 +0530

    feat: include attendance routes by requiring attendance.php in web.php

--- routes/web.php
+++ routes/web.php
@@ -69,6 +69,7 @@ Route::get("/departments/{id}/edit",[departmentController::class,"edit"])->name(
  //delete department route
  Route::delete("/departments/{id}",[departmentController::class,"destroy"])->name("departments.destroy");
 
+require __DIR__.'/admin/attendance.php';
 
 }); 
 
